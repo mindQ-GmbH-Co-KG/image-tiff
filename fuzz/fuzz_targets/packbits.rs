e866a149fdf1e0a04791a68fab11edbad163776a
Pretorer <39985339+Pretorer@users.noreply.github.com>
2022-01-12 14:57:29 +0100

    Revert "add fuzz-test"
    This reverts commit 58b7c4cdf029cc7414f1da13a121ecb80dffd030.

--- fuzz/fuzz_targets/packbits.rs
+++
@@ -1,8 +0,0 @@
-#![no_main]
-use libfuzzer_sys::fuzz_target;
-
-fuzz_target!(|data: &[u8]| {
-    let mut compressed_data = Vec::<u8>::new();
-    let mut writer = Cursor::new(&mut compressed_data);
-    Packbits::default().write_to(&mut writer, data).unwrap();
-});
